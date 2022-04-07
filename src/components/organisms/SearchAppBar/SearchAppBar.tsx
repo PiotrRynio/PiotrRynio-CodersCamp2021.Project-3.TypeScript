137b908be74769addc5958e91fc64c96bcaa0b1a
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-04-07 18:05:41 +0200

    in progress

--- src/components/organisms/SearchAppBar/SearchAppBar.tsx
+++ src/components/organisms/SearchAppBar/SearchAppBar.tsx
@@ -6,51 +6,9 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+import { SearchComponent } from "../../molecules/Search/Search";
 
 export const SearchAppBar = () => {
   return (
@@ -72,15 +30,7 @@ export const SearchAppBar = () => {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           ></Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+          <SearchComponent />
         </Toolbar>
       </AppBar>
     </Box>
--- src/components/pages/App/App.tsx
+++ src/components/pages/App/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./App.module.css";
 import { SearchAppBar } from "components/organisms/SearchAppBar/SearchAppBar";
+import { Box, Container } from "@mui/material";
 
 function App() {
   return (
     <>
-      <div className={styles.background}></div>
-      <SearchAppBar />
+      <Box className={styles.background}></Box>
+      <Container sx={{ marginY: 10 }}>
+        <SearchAppBar />
+      </Container>
     </>
   );
 }
