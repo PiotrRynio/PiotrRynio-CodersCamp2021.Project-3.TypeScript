f09ae1a39c7ba12eb739bdc8d708a104b884d5a7
Piotr Rynio <pwrynio@gmail.com>
2022-04-09 21:27:55 +0200

    feat(#12): clean files

--- src/components/molecules/Search/Search.tsx
+++ src/components/molecules/Search/Search.tsx
@@ -1,47 +1,12 @@
 import SearchIcon from "@mui/icons-material/Search";
-import { alpha, styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import styles from "./Search.module.css";
-
-/*const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));*/
-
-/*
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
-*/
 
 export const SearchComponent = () => {
   return (
     <div className={styles.search}>
       <div className={styles.searchIconWrapper}>
-        <SearchIcon />
+        <SearchIcon className={styles.searchIcon} />
       </div>
       <InputBase
         placeholder="Search..."
@@ -49,11 +14,5 @@ export const SearchComponent = () => {
         className={styles.searchInput}
       />
     </div>
-    /*      <StyledInputBase
-        placeholder="Search…"
-        inputProps={{ "aria-label": "search" }}
-      />*!/*/
-    //</Search>
-    //);
   );
 };
