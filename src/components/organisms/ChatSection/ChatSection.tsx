e7c29ff35d9c3a1d0ea4fd4f6d0474ea9f904962
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-04-10 15:27:03 +0200

    feat: add chat header

--- src/components/organisms/ChatSection/ChatSection.tsx
+++ src/components/organisms/ChatSection/ChatSection.tsx
@@ -2,25 +2,30 @@ import styles from "./ChatSection.module.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
+import Box from "@mui/material/Box";
 
 export const ChatSection = () => {
   return (
-    <div className={styles.newMessageSection}>
-      <TextField
-        className={styles.messageInput}
-        aria-label="maximum height"
-        placeholder="Enter message..."
-        variant="filled"
-        multiline
-        maxRows="6"
-      />
-      <Button
-        variant="contained"
-        startIcon={<SendIcon />}
-        className={styles.sendButton}
-      >
-        Send
-      </Button>
-    </div>
+    <>
+      <Box className={styles.chatHeader}>HEADER</Box>
+      <Box className={styles.newMessageSection}>
+        <TextField
+          className={styles.messageInput}
+          aria-label="maximum height"
+          placeholder="Enter message..."
+          variant="filled"
+          multiline
+          maxRows="6"
+        />
+
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          className={styles.sendButton}
+        >
+          Send
+        </Button>
+      </Box>
+    </>
   );
 };
