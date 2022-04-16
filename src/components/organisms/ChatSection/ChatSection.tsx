efb569129b9f046022c469fc5e866b3cfed944fc
Patryk Święcicki <patswiecicki@gmail.com>
2022-04-16 06:25:42 +0200

    refactor: Clear ChatSection

--- src/components/organisms/ChatSection/ChatSection.tsx
+++ src/components/organisms/ChatSection/ChatSection.tsx
@@ -22,31 +22,6 @@ export const ChatSection = () => {
           isLast={false}
           isOwn={true}
         />
-
-        <Message
-          author={{
-            name: "Patryk Święcicki",
-            avatar:
-              "https://gfx.planeta.pl/var/planetapl/storage/images/wiadomosci/swiat/aligator-zaatakowal-rowerzyste.-wzial-go-w-paszcze-21-07-2021/5276486-1-pol-PL/ALIGATOR-ZAATAKOWAL-ROWERZYSTE.-Wzial-go-w-paszcze_article.jpg",
-          }}
-          message={{ content: "Podoba się?", date: new Date() }}
-          isLast={true}
-          isOwn={true}
-        />
-
-        <Message
-          author={{
-            name: "Agnieszka Przybyłowska",
-            avatar:
-              "https://tueuropa.pl/uploads/articles_files/2021/11/05/6e7f9516-1948-d9e8-ca22-00007380aca5.jpg",
-          }}
-          message={{
-            content: "Hmmm... Muszę się zastanowić",
-            date: new Date(),
-          }}
-          isLast={true}
-          isOwn={false}
-        />
       </Box>
       <Box className={styles.newMessageSection}>
         <TextField
