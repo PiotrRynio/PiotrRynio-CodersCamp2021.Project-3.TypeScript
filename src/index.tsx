edda7748e0ad83dc8d1eab3315f066b2710d20ff
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-04-18 01:00:00 +0200

    Login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import { App } from "./components/pages/App/App";
+import { Login } from "./components/pages/Login/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById("root")
 );
