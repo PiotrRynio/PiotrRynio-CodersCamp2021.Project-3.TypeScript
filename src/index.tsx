0f0b5132f8edca3a74e7c776e6cba97fa7a7ba7e
Patryk Święcicki <patswiecicki@gmail.com>
2022-04-18 11:19:35 +0200

    refactor: remove WelcomePage from main index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "components";
-import { WelcomePage } from "./components/pages/WelcomePage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <WelcomePage />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
