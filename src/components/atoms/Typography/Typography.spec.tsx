7b33a2da4b6671ce520071cdb75b9b2d2c3676dd
Piotr Rynio <pwrynio@gmail.com>
2022-04-22 18:50:47 +0200

    test(#37): add test

--- src/components/atoms/Typography/Typography.spec.tsx
+++ src/components/atoms/Typography/Typography.spec.tsx
@@ -1,12 +1,17 @@
 import { render, screen } from "test-utils";
 import { Typography } from "./Typography";
+import { TypographyVariant } from "./TypographyVariant";
 
-describe("Typography component", () => {
+describe("<Typography> component", () => {
   const testText = "test text";
 
-  it("should display REGULAR text, if component is rendered", () => {
+  it("should display MessageContent text, if component is rendered", () => {
     // when
-    render(<Typography>{testText}</Typography>);
+    render(
+      <Typography variant={TypographyVariant.MESSAGE_CONTENT}>
+        {testText}
+      </Typography>
+    );
 
     // then
     const textComponent = screen.getByText(/test text/i);
