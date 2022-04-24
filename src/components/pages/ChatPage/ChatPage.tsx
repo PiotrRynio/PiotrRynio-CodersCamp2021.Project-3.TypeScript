e36f6c87fb3bb71cf6834f0044c151fc11abed2f
Patryk Święcicki <patswiecicki@gmail.com>
2022-04-24 21:09:13 +0200

    Add logout button for phones

--- src/components/pages/ChatPage/ChatPage.tsx
+++ src/components/pages/ChatPage/ChatPage.tsx
@@ -13,11 +13,11 @@ export const ChatPage = () => {
   const navigate = useNavigate();
   const { currentUser } = useAuth();
 
-  /*  useEffect(() => {
+  useEffect(() => {
     if (!currentUser) {
       navigate("/login");
     }
-  }, []);*/
+  }, []);
 
   return (
     <Box className={styles.background}>
